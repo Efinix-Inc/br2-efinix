20e76405b4de73088ea5888f39d3e7fc3f149a3d
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2022-07-19 14:45:00 +0800

    init.sh: add a script to simplify the build flow
    User just need to run the init script to start building
    linux image. The init script would clone the Buildroot
    repository, generate the device tree and prepare the
    build environment.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

---
+++ init.sh
@@ -0,0 +1,112 @@
+#!/bin/bash -e
+
+# args1: board
+# args2: path/to/soc.h
+
+BOARD=$1
+SOC_H=$2
+
+INPUT_FILE="VERSION"
+JSON_FILE="boards/efinix/common/drivers.json"
+devkits=$(jq '.devkits | .[]' $JSON_FILE)
+
+arr=()
+substr="buildroot"
+buildroot_version=''
+BUILDROOT_REPO="https://github.com/buildroot/buildroot.git"
+
+PROJ_DIR=$PWD
+BR2_EXTERNAL_DIR=$PROJ_DIR
+BUILDROOT_DIR="$PROJ_DIR/../buildroot"
+BUILD_DIR="$PROJ_DIR/../build"
+BUILDROOT_DEFCONFIG=""
+
+function usage()
+{
+	echo "$0 <board [t120f324|ti60f225]> <path/to/soc.h>"
+	echo
+	echo "Example,"
+	echo "$0 t120f324 ~/efinity/2022.1/project/soc/ip/soc1/T120F324_devkit/embedded_sw/soc1/bsp/efinix/EfxSapphireSoc/include/soc.h"
+}
+
+if [[ -z $BOARD ]]; then
+	echo Error: Board is not defined
+	usage
+	return
+fi
+
+if [[ -z $SOC_H ]]; then
+	echo Error: soc.h is not defined
+	usage
+	return
+fi
+
+# check the compatible of $BOARD in $JSON_FILE
+found=false
+for devkit in ${devkits[@]}; do
+        if [[ $devkit == *$BOARD* ]]; then
+                BOARD=$(echo $devkit | tr -d \")
+                BUILDROOT_DEFCONFIG="efinix_"$BOARD"_defconfig"
+		found=true
+		break
+        fi
+done
+
+if [[ $found == false ]]; then
+	echo Error: board $BOARD is not supported
+	return
+fi
+
+# Read the VERSION file to get buildroot version
+while IFS= read -r line; do
+	if [[ $line == *$substr* ]];
+	then
+		IFS=' ' read -r -a arr <<< "$line"
+	fi
+done < $INPUT_FILE
+
+buildroot_version=${arr[1]}
+
+# clone buildroot repository
+if [[ ! -z $buildroot_version ]]; then
+	echo Using Buildroot $buildroot_version
+	git clone $BUILDROOT_REPO -b $buildroot_version
+else
+	echo Using Buildroot master/main branch
+	git clone $BUILDROOT_REPO
+fi
+
+mv buildroot $BUILDROOT_DIR
+
+# copy soc.h to opensbi directory. Opensbi has dependency on soc.h.
+OPENSBI_DIR="$BR2_EXTERNAL_DIR/boards/efinix/$BOARD/opensbi"
+cp $SOC_H $OPENSBI_DIR
+
+SOC_H=$OPENSBI_DIR/soc.h
+
+# count number of cpu core and modify soc.h
+cpu_count=1
+for i in {4..1}; do
+        num=$(( $i-1))
+        substr="SYSTEM_PLIC_SYSTEM_CORES_${num}_EXTERNAL_INTERRUPT"
+        if [[ $(cat $SOC_H | grep $substr) ]]; then
+            cpu_count=$i
+            break
+        fi
+done
+
+echo "Detecting $cpu_count RISC-V CPU cores"
+
+# modify soc.h by appending SYSTEM_CORES_COUNT
+IFS=$'\n' read -d '' -r -a lines < $SOC_H
+lines[${#lines[@]}-1]="#define SYSTEM_CORES_COUNT $cpu_count"
+lines[${#lines[@]}]="#endif"
+printf "%s\n" "${lines[@]}" > ${SOC_H}
+
+# generate device tree
+python3 boards/efinix/common/device_tree_generator.py $SOC_H $BOARD
+
+# prepare Buildroot build environment
+mkdir $BUILD_DIR
+cd $BUILD_DIR && \
+make O=$PWD BR2_EXTERNAL=$BR2_EXTERNAL_DIR -C $BUILDROOT_DIR $BUILDROOT_DEFCONFIG
