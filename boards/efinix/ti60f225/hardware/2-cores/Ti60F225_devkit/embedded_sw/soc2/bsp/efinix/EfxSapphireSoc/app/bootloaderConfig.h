392195226ae9370fa74ad9d9a091debcfbd2638a
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2022-08-16 11:40:21 +0800

    ti60f225: add soc example design with 2 cores
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

---
+++ boards/efinix/ti60f225/hardware/2-cores/Ti60F225_devkit/embedded_sw/soc2/bsp/efinix/EfxSapphireSoc/app/bootloaderConfig.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "bsp.h"
+#include "io.h"
+#include "spiFlash.h"
+#include "riscv.h"
+#include "start.h"
+
+#define SPI SYSTEM_SPI_0_IO_CTRL
+#define SPI_CS 0
+
+#define OPENSBI_MEMORY    0x01000000
+#define OPENSBI_FLASH     0x00400000
+#define OPENSBI_SIZE      0x040000
+
+#define UBOOT_MEMORY      0x01040000
+#define UBOOT_SBI_FLASH   0x00480000
+#define UBOOT_SIZE        0x0C0000
+
+void bspMain()
+{
+#ifndef SPINAL_SIM
+    spiFlash_init(SPI, SPI_CS);
+    spiFlash_wake(SPI, SPI_CS);
+    bsp_putString("OpenSBI copy\r\n");
+    spiFlash_f2m(SPI, SPI_CS, OPENSBI_FLASH, OPENSBI_MEMORY, OPENSBI_SIZE);
+    bsp_putString("U-Boot copy\r\n");
+    spiFlash_f2m(SPI, SPI_CS, UBOOT_SBI_FLASH, UBOOT_MEMORY, UBOOT_SIZE);
+#endif
+
+    bsp_putString("Payload boot\r\n");
+    void (*userMain)(u32, u32, u32) = (void (*)(u32, u32, u32))OPENSBI_MEMORY;
+    #ifdef SMP
+    smp_unlock(userMain);
+    #endif
+    userMain(0, 0, 0);
+}
