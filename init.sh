a26cbbc9b16f6c849eb5cde8adca69f60528ea12
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2022-11-08 14:24:00 +0800

    init.sh: add optional arguments and update the script
    Optional arguments for rename build directory and reconfigure
    the Buildroot build directory is very useful when working with
    multiple build directory or debugging.
    
    Also fixed the Linux configuration by turning off the CONFIG_FPU
    option when the soc.h does not support it.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- init.sh
+++ init.sh
@@ -3,9 +3,12 @@
 # args1: board
 # args2: path/to/soc.h
 
+OPTIND=1
+unset RECONFIGURE
+unset OPT_DIR
+
 BOARD=$1
 SOC_H=$2
-OPT_DIR=$3
 
 INPUT_FILE="VERSION"
 JSON_FILE="boards/efinix/common/drivers.json"
@@ -22,39 +25,157 @@ BUILDROOT_DEFCONFIG=""
 
 function usage()
 {
-	echo "$0 <board [t120f324|ti60f225]> <path/to/soc.h> <optional workdir>"
+	echo "Usage: $0 [board <t120f324|ti60f225>] [path/to/soc.h <string> ] [-d build directory <string>] [-r reconfigure]"
 	echo
-	echo "Example,"
+	echo "Positional arguments:"
+	echo "	board			development kit name such as t120f324, ti60f225"
+	echo "	soc			path to soc.h. This file is located in Efinity project directory"
+	echo
+	echo "Optional arguments:"
+	echo "	-h			Show this help message and exit"
+	echo "	-d			Rename default build directory name"
+	echo "				By default is <board>_build"
+	echo "				Example, if board is ti60f225 then name of build directory is ti60f225_build"
+	echo "	-r			Reconfigure the Buildroot configuration"
+
+	echo "Example usage,"
 	echo "$0 t120f324 ~/efinity/2022.1/project/soc/ip/soc1/T120F324_devkit/embedded_sw/soc1/bsp/efinix/EfxSapphireSoc/include/soc.h"
 }
 
+function sanity_check()
+{
+	if [[ ! -f $SOC_H ]]; then
+		echo "Error: $SOC_H file not exists"
+		return
+	fi
+
+	# check the compatible of $BOARD in $JSON_FILE
+	found=false
+	for devkit in ${devkits[@]}; do
+		if [[ $devkit == *$BOARD* ]]; then
+			BOARD=$(echo $devkit | tr -d \")
+			BUILDROOT_DEFCONFIG="efinix_"$BOARD"_defconfig"
+			found=true
+			break
+		fi
+	done
+
+	if [[ $found == false ]]; then
+		echo Error: board $BOARD is not supported
+		return
+	fi
+
+	# check for SPI1. SPI1 is used by the SD card to store Linux image
+	if [[ -z $(cat $SOC_H | grep SYSTEM_SPI_1_IO_CTRL) ]]; then
+		echo Error: The generated SoC does not enable SPI1.
+		echo        Please regenerate the SoC with SPI1.
+		return
+	fi
+}
+
+function modify_soc_h()
+{
+	# modify soc.h by appending SYSTEM_CORES_COUNT
+	if [[ ! $(cat $SOC_H | grep SYSTEM_CORES_COUNT) ]]; then
+		IFS=$'\n' read -d '' -r -a lines < $SOC_H
+		lines[${#lines[@]}-1]="#define SYSTEM_CORES_COUNT $cpu_count"
+		lines[${#lines[@]}]="#endif"
+		printf "%s\n" "${lines[@]}" > ${SOC_H}
+	fi
+
+	# check for floating point from soc.h and modify buildroot defconfig
+	fp=$(cat ${SOC_H} | grep FPU | awk  '{print $3}' | head -1)
+	if [[ $fp == 0 ]]; then
+		# disable the floating point
+		echo "INFO: Disable floating point in $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG"
+		sed -i 's/^BR2_RISCV_ISA_CUSTOM_RVF=y/BR2_RISCV_ISA_CUSTOM_RVF=n/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
+		sed -i 's/^BR2_RISCV_ISA_CUSTOM_RVD=y/BR2_RISCV_ISA_CUSTOM_RVD=n/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
+		sed -i 's/^BR2_RISCV_ABI_ILP32D=y/BR2_RISCV_ABI_ILP32=y/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
+
+		echo "INFO: Disable Linux FPU support in $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config"
+		sed -i 's/^CONFIG_FPU=y/CONFIG_FPU=n/g' $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config
+	fi
+}
+
+function generate_device_tree()
+{
+	# generate device tree
+	python3 boards/efinix/common/device_tree_generator.py $SOC_H $BOARD
+}
+
+function prepare_buildroot_env()
+{
+	# prepare Buildroot build environment
+	mkdir $BUILD_DIR
+	cd $BUILD_DIR && \
+	make O=$PWD BR2_EXTERNAL=$BR2_EXTERNAL_DIR -C $BUILDROOT_DIR $BUILDROOT_DEFCONFIG
+}
+
+function get_cpu_count()
+{
+	# count number of cpu core
+	cpu_count=1
+	for i in {4..1}; do
+		num=$(( $i-1))
+		substr="SYSTEM_PLIC_SYSTEM_CORES_${num}_EXTERNAL_INTERRUPT"
+		if [[ $(cat $SOC_H | grep $substr) ]]; then
+			cpu_count=$i
+			break
+		fi
+	done
+}
+
+function check_smp()
+{
+	get_cpu_count
+	echo "INFO: Detecting $cpu_count RISC-V CPU cores"
+	if [[ $cpu_count -gt 1 ]]; then
+		# enable CONFIG_SMP=y in linux.config
+		sed -i 's/^CONFIG_SMP=n/CONFIG_SMP=y/g' $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config
+	else
+		sed -i 's/^CONFIG_SMP=y/CONFIG_SMP=n/g' $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config
+	fi
+}
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-*) break
+		;;
+	esac
+	shift
+done
+
+while getopts ":d:r:h" o; do
+	case "${o}" in
+		d)
+			OPT_DIR=${OPTARG}
+			;;
+		r)
+			RECONFIGURE=1
+			;;
+		h)	usage
+			return
+			;;
+		\?)
+			echo "ERROR: Invalid option -$OPTARG"
+			usage
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
 if [[ -z $BOARD ]]; then
 	echo Error: Board is not defined
-	usage
 	return
 fi
 
 if [[ -z $SOC_H ]]; then
 	echo Error: soc.h is not defined
-	usage
 	return
 fi
 
-# check the compatible of $BOARD in $JSON_FILE
-found=false
-for devkit in ${devkits[@]}; do
-        if [[ $devkit == *$BOARD* ]]; then
-                BOARD=$(echo $devkit | tr -d \")
-                BUILDROOT_DEFCONFIG="efinix_"$BOARD"_defconfig"
-		found=true
-		break
-        fi
-done
-
-if [[ $found == false ]]; then
-	echo Error: board $BOARD is not supported
-	return
-fi
+sanity_check
 
 WORKSPACE="build_$BOARD"
 
@@ -65,6 +186,23 @@ fi
 WORKSPACE_DIR="$PROJ_DIR/../$WORKSPACE"
 BUILDROOT_DIR="$PROJ_DIR/../$WORKSPACE/buildroot"
 BUILD_DIR="$PROJ_DIR/../$WORKSPACE/build"
+OPENSBI_DIR="$BR2_EXTERNAL_DIR/boards/efinix/$BOARD/opensbi"
+
+if [[ $RECONFIGURE == 1 ]]; then
+	# reconfigure the .config
+	SOC_H=$OPENSBI_DIR/soc.h
+
+	if [[ -d "$BUILD_DIR" ]]; then
+		check_smp
+		modify_soc_h
+		generate_device_tree
+		cd $BUILD_DIR && \
+		make O=$PWD BR2_EXTERNAL=$BR2_EXTERNAL_DIR -C $BUILDROOT_DIR $BUILDROOT_DEFCONFIG 
+	else
+		echo "Error: $BUILD_DIR not exist"
+	fi
+	return
+fi
 
 mkdir -p $WORKSPACE_DIR
 
@@ -90,57 +228,10 @@ fi
 mv buildroot $BUILDROOT_DIR
 
 # copy soc.h to opensbi directory. Opensbi has dependency on soc.h.
-OPENSBI_DIR="$BR2_EXTERNAL_DIR/boards/efinix/$BOARD/opensbi"
 cp $SOC_H $OPENSBI_DIR/soc.h
-
 SOC_H=$OPENSBI_DIR/soc.h
 
-# check for SPI1
-if [[ -z $(cat $SOC_H | grep SYSTEM_SPI_1_IO_CTRL) ]]; then
-	echo Error: The generated SoC does not enable SPI1.
-	echo        Please regenerate the SoC with SPI1.
-	return
-fi
-
-# count number of cpu core and modify soc.h
-cpu_count=1
-for i in {4..1}; do
-        num=$(( $i-1))
-        substr="SYSTEM_PLIC_SYSTEM_CORES_${num}_EXTERNAL_INTERRUPT"
-        if [[ $(cat $SOC_H | grep $substr) ]]; then
-            cpu_count=$i
-            break
-        fi
-done
-
-echo "Detecting $cpu_count RISC-V CPU cores"
-if [[ $cpu_count -gt 1 ]]; then
-	# enable CONFIG_SMP=y in linux.config
-	sed -i 's/^CONFIG_SMP=n/CONFIG_SMP=y/g' $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config
-else
-	sed -i 's/^CONFIG_SMP=y/CONFIG_SMP=n/g' $BR2_EXTERNAL_DIR/boards/efinix/$BOARD/linux/linux.config
-fi
-
-# modify soc.h by appending SYSTEM_CORES_COUNT
-IFS=$'\n' read -d '' -r -a lines < $SOC_H
-lines[${#lines[@]}-1]="#define SYSTEM_CORES_COUNT $cpu_count"
-lines[${#lines[@]}]="#endif"
-printf "%s\n" "${lines[@]}" > ${SOC_H}
-
-# check for floating point from soc.h
-fp=$(cat ${SOC_H} | grep FPU | awk  '{print $3}')
-if [[ $fp == 0 ]]; then
-	# disable the floating point
-	echo disable floating point in $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
-	sed -i 's/^BR2_RISCV_ISA_CUSTOM_RVF=y/BR2_RISCV_ISA_CUSTOM_RVF=n/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
-	sed -i 's/^BR2_RISCV_ISA_CUSTOM_RVD=y/BR2_RISCV_ISA_CUSTOM_RVD=n/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
-	sed -i 's/^BR2_RISCV_ABI_ILP32D=y/BR2_RISCV_ABI_ILP32=y/g' $BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG
-fi
-
-# generate device tree
-python3 boards/efinix/common/device_tree_generator.py $SOC_H $BOARD
-
-# prepare Buildroot build environment
-mkdir $BUILD_DIR
-cd $BUILD_DIR && \
-make O=$PWD BR2_EXTERNAL=$BR2_EXTERNAL_DIR -C $BUILDROOT_DIR $BUILDROOT_DEFCONFIG
+check_smp
+modify_soc_h
+generate_device_tree
+prepare_buildroot_env
