33c672c3e51154d8decec2725c1bcf22f9279490
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2024-05-09 13:43:23 +0800

    common: change the printing API for bootloader
    The baremetal bootloader API has changed to
    bsp_printf().
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- boards/efinix/common/bootloaderConfig.h
+++ boards/efinix/common/bootloaderConfig.h
@@ -38,13 +38,13 @@ void bspMain()
 #ifndef SPINAL_SIM
     spiFlash_init(SPI, SPI_CS);
     spiFlash_wake(SPI, SPI_CS);
-    bsp_putString("OpenSBI copy\r\n");
+    bsp_printf("OpenSBI copy\r\n");
     spiFlash_f2m(SPI, SPI_CS, OPENSBI_FLASH, OPENSBI_MEMORY, OPENSBI_SIZE);
-    bsp_putString("U-Boot copy\r\n");
+    bsp_printf("U-Boot copy\r\n");
     spiFlash_f2m(SPI, SPI_CS, UBOOT_SBI_FLASH, UBOOT_MEMORY, UBOOT_SIZE);
 #endif
 
-    bsp_putString("Payload boot\r\n");
+    bsp_printf("Payload boot\r\n");
     void (*userMain)(u32, u32, u32) = (void (*)(u32, u32, u32))OPENSBI_MEMORY;
     #ifdef SMP
     smp_unlock(userMain);
