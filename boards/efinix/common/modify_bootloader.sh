c700de75ebb81779fb6e79328400eff1adc1002e
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2024-05-20 23:42:08 +0800

    modify_bootloader.sh: refactor the code
    There are a few changes in this patch.
    
    1. Remove the argument for Efinity installation directory.
    The bin file is no longer generated by this script.
    
    2. Remove bin directory from the IDE argument. Now, user only
    need to give the RISCV IDE installation directory.
    For example, ~/workspace/efinity/efinity-riscv-ide-2023.2.1.1
    instead of ~/workspace/efinity/efinity-riscv-ide-2023.2.1.1/bin
    
    3. Add optional arguments such as debug (-d) and example design (-e).
    The example design (-e) argument is use when user want to compile
    the bootloader from example design directory instead of project directory.
    For example,
    The project directory is ~/efinity/project/my_project.
    The example design project would be ~/efinity/project/my_project/ip/<example design>.
    
    4. Ti375C529 use different directory structure for embedded_sw.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- boards/efinix/common/modify_bootloader.sh
+++ boards/efinix/common/modify_bootloader.sh
@@ -3,32 +3,71 @@
 # $1 = development board i.e. t120f324, ti60f225
 # $2 = path to Efinity project
 # $3 = path to RISC-V SDK
-# $4 = efinity installation directory
 
 DEVKIT=$1
 EFINITY_PROJECT=$2
 RISCV_IDE=$3
-EFINITY_HOME=$4
 
-JSON_FILE="drivers.json"
-DEVKITS=$(jq '.devkits | .[]' $JSON_FILE)
+unset EXAMPLE_DESIGN
+unset DEBUG
 
 function usage()
 {
-	echo "This script used to modify bootloader program for booting Linux."
 	echo
-	echo "command"
-	echo "$0 <devkit> <efinity project directory> <RISCV IDE directory> <Efinity installation directory>"
+	echo "This script used to modify and compile the first stage bootloader program for booting Linux."
 	echo
-	echo "supported <devkit> are t120f324, ti60f225"
-	echo "<Efinity project directory> can be $HOME/soc/ip/soc1"
-	echo "<RISCV IDE> can be $HOME/efinity/efinity-riscv-ide-2022.2.3"
-	echo "<Efinity installation directory> can be $HOME/efinity/2022.1/bin"
+	echo "usage: $0 Devkit Project IDE [ -e][-d]"
+	echo "Positional arguments:"
+	echo "	Devkit		Supported devkits are t120f324, ti60f225, ti180j484, ti375c529"
+	echo "	Project		Efinity project directory. For example, $HOME/soc"
+	echo "	IDE		RISCV IDE installation directory. For example,"
+	echo "			$HOME/efinity/efinity-riscv-ide-2022.2.3"
 	echo
-	echo "Example command for t120f324 devkit"
-	echo "$0 t120f324 $HOME/soc/ip/soc1 $HOME/efinity/efinity-riscv-ide-2022.2.3 $HOME/efinity/2022.1/bin"
+	echo "Optional arguments:"
+	echo "	-d		Show debug message"
+	echo "	-e		Use example design project of Sapphire SoC."
+	echo "			It will use the Efinity project from"
+	echo "			Project/ip/<sapphire soc>/<Devkit>_devkit/"
+	echo "	-h		Show this message"
+	echo
+	echo "Example,"
+	echo "Modify bootloader example design of T120F324"
+	echo "$0 t120f324 $HOME/soc $HOME/efinity/efinity-riscv-ide-2022.2.3 -e"
 	exit
 }
+
+while [ $# -gt 0 ]; do
+        case $1 in
+                -*)
+		break
+                ;;
+        esac
+        shift
+done
+
+while getopts ":deh" o; do
+        case "${o}" in
+        :)
+                echo "ERROR: Option -$OPTARG requires an argument"
+                ;;
+	d)
+		echo enable debug message
+		DEBUG=1
+		;;
+	e)
+		echo using example design
+		EXAMPLE_DESIGN=1
+		;;
+        h)
+                usage
+                ;;
+        \?)
+                echo "ERROR: Invalid option -$OPTARG"
+                usage
+                ;;
+        esac
+done
+shift $((OPTIND-1))
 
 if [[ -z $DEVKIT ]]; then
 	echo Error: Devkit is not set
@@ -45,14 +84,9 @@ if [[ -z $RISCV_IDE ]]; then
 	usage
 fi
 
-if [[ -z $EFINITY_HOME ]]; then
-	echo Error: EFINITY_HOME is not set
-	usage
-fi
-
-source $EFINITY_HOME/setup.sh
-
 DEVKIT=$(echo $DEVKIT | tr '[:upper:]' '[:lower:]')
+JSON_FILE="sapphire-soc-dt-generator/config/drivers.json"
+DEVKITS=$(jq '.devkits.Titanium, .devkits.Trion | .[]' $JSON_FILE)
 
 found=false
 for devkit in ${DEVKITS[@]}; do
@@ -72,13 +106,61 @@ fi
 
 echo Info: $DEVKIT devkit is supported
 
-IFS='/' read -ra ARRAY <<< "$EFINITY_PROJECT"
-EFINITY_PROJECT_NAME=${ARRAY[-1]}
+SAPPHIRE_IP=
 
-EFINITY_PROJECT_PATH="$EFINITY_PROJECT/$DEVKIT/embedded_sw/$EFINITY_PROJECT_NAME"
+if [[ $EXAMPLE_DESIGN ]];then
+	DEVKIT_DIR=$DEVKIT
+	if [[ "Ti375C529_devkit" == *$DEVKIT* ]]; then
+		EXAMPLE_DESIGN_DIR="/ip/EfxSapphireHpSoc_slb/$DEVKIT_DIR"
+	else
+		tmp=$(find $EFINITY_PROJECT -type d -name $DEVKIT_DIR)
+		if [ -z $tmp ]; then
+			echo Error: Example design for $DEVKIT is not exists
+			exit 1
+		fi
+
+		EXAMPLE_DESIGN_DIR=${tmp//$EFINITY_PROJECT}
+		ip_name=${EXAMPLE_DESIGN_DIR//$DEVKIT_DIR}
+		ip_name=${ip_name//"/ip/"}
+		ip_name=${ip_name//"/"}
+		EFINITY_PROJECT_NAME=$ip_name
+		SAPPHIRE_IP=$ip_name
+
+	fi
+else
+	DEVKIT_DIR=
+	EXAMPLE_DESIGN_DIR=""
+	tmp=$(find $EFINITY_PROJECT -type d -name bsp)
+	embedded_sw="$EFINITY_PROJECT/embedded_sw/"
+	ip_name=${tmp//"/bsp"}
+	ip_name=${ip_name//$embedded_sw}
+	SAPPHIRE_IP=$ip_name
+
+fi
+
+if [[ "Ti375C529_devkit" == *$DEVKIT* ]]; then
+	EFINITY_PROJECT_PATH="$EFINITY_PROJECT$EXAMPLE_DESIGN_DIR/embedded_sw/efx_hard_soc"
+else
+	EFINITY_PROJECT_PATH="$EFINITY_PROJECT$EXAMPLE_DESIGN_DIR/embedded_sw/$SAPPHIRE_IP"
+fi
+
 EfxSapphireSoc_DIR="$EFINITY_PROJECT_PATH/bsp/efinix/EfxSapphireSoc"
 APP_DIR="$EfxSapphireSoc_DIR/app"
 SOC_H=$EfxSapphireSoc_DIR/include/soc.h
+
+if [ $DEBUG ]; then
+	echo
+	echo "******** DEBUG *******"
+	echo EFINITY_PROJECT=$EFINITY_PROJECT
+	echo EXAMPLE_DESIGN_DIR=$EXAMPLE_DESIGN_DIR
+	echo DEVKIT_DIR=$DEVKIT_DIR
+	echo EFINITY_PROJECT_PATH=$EFINITY_PROJECT_PATH
+	echo EfxSapphireSoc_DIR=$EfxSapphireSoc_DIR
+	echo APP_DIR=$APP_DIR
+	echo SOC_H=$SOC_H
+	echo "***********************"
+	echo
+fi
 
 RISCV_TOOLCHAIN="efinity-riscv-ide-[0-9]+\.[0-9]+\.[0-9]"
 
@@ -90,8 +172,14 @@ else
 	exit
 fi
 
-# copy bootloaderConfig.h to $APP_DIR
-cp bootloaderConfig.h $APP_DIR
+BOOTLOADER_DIR=$EFINITY_PROJECT_PATH/software/standalone/bootloader
+echo BOOTLOADER_DIR=$BOOTLOADER_DIR
+if [[ "Ti375C529_devkit" == *$DEVKIT* ]]; then
+	cp bootloaderConfig.h $BOOTLOADER/src
+	echo $BOOTLOADER_SRC
+else
+	cp bootloaderConfig.h $APP_DIR
+fi
 
 # check for SMP
 SMP=$(cat $SOC_H | grep SYSTEM_PLIC_SYSTEM_CORES_1_EXTERNAL_INTERRUPT)
@@ -104,16 +192,14 @@ fi
 cd $EFINITY_PROJECT_PATH/software/standalone/bootloader && \
 	BSP_PATH=$EfxSapphireSoc_DIR make clean && \
 	BSP_PATH=$EfxSapphireSoc_DIR make
-cd -
 
-FPU=$(cat $SOC_H | grep SYSTEM_CORES_0_FPU | awk '{print $3}')
-RAM_SIZE=$(cat $SOC_H | grep SYSTEM_RAM_A_SIZE | awk '{print $3}')
+	if [ ! $? -eq 0 ]; then
+		echo Error: compilation failed
+		cd -
+		exit 1
+	fi
+cd - > /dev/null
 
-# generate new ram
-python3 $EFINITY_PROJECT_PATH/tool/binGen.py -f $FPU -s $RAM_SIZE -b $EFINITY_PROJECT_PATH/software/standalone/bootloader/build/bootloader.bin
-
-# copy new ram to EFINITY_PROJECT
-cp -r rom/* $EFINITY_PROJECT/$DEVKIT
-rm -rf rom
-
+cp -r $BOOTLOADER_DIR/build $EFINITY_PROJECT/linux_bootloader
+echo The bootloader is in $EFINITY_PROJECT/linux_bootloader/bootloader.hex
 echo done
