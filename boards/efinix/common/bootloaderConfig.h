5d68c28f73d5aef59782114bb52d9962e74ad292
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-03-07 16:03:07 +0800

    bootloaderConfig.h: change printing API to use bsp_print_s()
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- boards/efinix/common/bootloaderConfig.h
+++ boards/efinix/common/bootloaderConfig.h
@@ -38,13 +38,13 @@ void bspMain()
 #ifndef SPINAL_SIM
     spiFlash_init(SPI, SPI_CS);
     spiFlash_wake(SPI, SPI_CS);
-    bsp_putString("OpenSBI copy\r\n");
+    bsp_printf_s("OpenSBI copy\r\n");
     spiFlash_f2m(SPI, SPI_CS, OPENSBI_FLASH, OPENSBI_MEMORY, OPENSBI_SIZE);
-    bsp_putString("U-Boot copy\r\n");
+    bsp_printf_s("U-Boot copy\r\n");
     spiFlash_f2m(SPI, SPI_CS, UBOOT_SBI_FLASH, UBOOT_MEMORY, UBOOT_SIZE);
 #endif
 
-    bsp_putString("Payload boot\r\n");
+    bsp_printf_s("Payload boot\r\n");
     void (*userMain)(u32, u32, u32) = (void (*)(u32, u32, u32))OPENSBI_MEMORY;
     #ifdef SMP
     smp_unlock(userMain);
