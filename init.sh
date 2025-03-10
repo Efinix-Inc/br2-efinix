20f623fe025e9045ee31e4c7dbfb1d3256319621
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-03-10 16:13:44 +0800

    init.sh: fix adding evsoc package
    Fix the checking of UNIFIED_HW variable before add the package.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- init.sh
+++ init.sh
@@ -410,8 +410,10 @@ function add_packages()
 	local br2_defconfig_path="$BR2_EXTERNAL_DIR/configs"
         local br2_defconfig="$BR2_EXTERNAL_DIR/configs/$BUILDROOT_DEFCONFIG"
 
-        [ $UNIFIED_HW ] && grep -q EVSOC $br2_defconfig || \
-	cat $br2_defconfig_path/evsoc_fragment >> $br2_defconfig
+        if [ $UNIFIED_HW ]; then
+		grep -q EVSOC $br2_defconfig || \
+		cat $br2_defconfig_path/evsoc_fragment >> $br2_defconfig
+	fi
 
         if [ $X11_GRAPHICS ]; then
                 grep -q BR2_PACKAGE_DESKTOP_ENVIRONMENT $br2_defconfig || \
