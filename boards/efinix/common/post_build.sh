4975116c8abe1c7ab7319d5e6c703431e14bca67
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-07-25 17:28:45 +0800

    common/post_build.sh: handle 32 and 64 bits objcopy
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- boards/efinix/common/post_build.sh
+++ boards/efinix/common/post_build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 LINUX_ADDRESS=0x00400000
-$HOST_DIR/bin/riscv32-buildroot-linux-gnu-objcopy  -O binary $BINARIES_DIR/vmlinux $BINARIES_DIR/Image
+
+# Set default objcopy binary
+objcopy_bin="riscv32-buildroot-linux-gnu-objcopy"
+vmlinux_file="$BINARIES_DIR/vmlinux"
+machine_arch=$(file "$vmlinux_file" | grep -o '[0-9]\+-bit')
+
+case "$machine_arch" in
+	"64-bit")
+		objcopy_bin="riscv64-buildroot-linux-gnu-objcopy"
+		;;
+	"32-bit")
+		objcopy_bin="riscv32-buildroot-linux-gnu-objcopy"
+		;;
+	*)
+		echo "Unsupported architecture: $machine_arch"
+		echo "Use default objcopy: $objcopy_bin"
+		;;
+esac
+
+$objcopy_bin -O binary "$vmlinux_file" "$BINARIES_DIR/Image"
 $HOST_DIR/bin/mkimage -A riscv -O linux -T kernel -C none -a $LINUX_ADDRESS -e $LINUX_ADDRESS -n Linux -d $BINARIES_DIR/Image $BINARIES_DIR/uImage
