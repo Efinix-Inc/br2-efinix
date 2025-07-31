1ce733b81984c2d4d4b4de002c6e1226be77b887
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-07-31 18:11:06 +0800

    modify_bootloader.sh: create a selection for bsp directory
    When user upgrade the SoC using Efinity software, it would create
    another bsp directory. This would confuse the script to select
    the correct bsp directory for compiling the bootloader.
    
    This patch let user to decide the correct bsp directory for
    compiling the bootloader.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- boards/efinix/common/modify_bootloader.sh
+++ boards/efinix/common/modify_bootloader.sh
@@ -134,20 +134,16 @@ if [[ $found == false ]]; then
 fi
 
 echo INFO: Checking RISC-V Toolchain
-RISCV_TOOLCHAIN="efinity-riscv-ide"
+RISCV_TOOLCHAIN_DIR="toolchain/bin"
 
-if [[ $RISCV_IDE =~ $RISCV_TOOLCHAIN ]]; then
-        RISCV_IDE=$RISCV_IDE/toolchain/bin
-        $RISCV_IDE/riscv-none-embed-gcc -dumpversion > /dev/null 2>&1
-        if [[ $? -eq 0 ]]; then
-                export PATH=$RISCV_IDE:$PATH
-        else
-                echo ERROR: RISCV_IDE path is invalid
-                exit 1
-        fi
+toolchain=$(find "$RISCV_IDE/$RISCV_TOOLCHAIN_DIR" -name *-gcc)
+$toolchain -dumpversion > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	export PATH=$RISCV_IDE/$RISCV_TOOLCHAIN_DIR:$PATH
+	echo "INFO: Found toolchain in $toolchain"
 else
-        echo ERROR: RISCV_IDE path is invalid
-        exit 1
+	echo "ERROR: $RISCV_IDE path is invalid"
+	exit 1
 fi
 
 if [[ "$EFINITY_PROJECT" != /* ]]; then
@@ -162,8 +158,36 @@ if [[ $EXAMPLE_DESIGN ]]; then
 fi
 
 EMBEDDED_SW_DIR="${EFINITY_PROJECT_DIR}/embedded_sw"
-BSP_DIR="$(find "${EMBEDDED_SW_DIR}" -type d -name bsp)"
-STANDALONE_DIR="$(find "${EMBEDDED_SW_DIR}" -type d -name standalone)"
+mapfile -t BSP_DIRS < <(find "${EMBEDDED_SW_DIR}" -type d -name bsp)
+
+echo "INFO: Found ${#BSP_DIRS[@]} BSP directories"
+if [ ${#BSP_DIRS[@]} -gt 1 ]; then
+	for i in "${!BSP_DIRS[@]}"; do
+		echo "$i: ${BSP_DIRS[$i]}"
+	done
+
+	# Prompt user for input
+	read -p "Select the BSP directory by the number: " selection
+
+	# Validate and use the selection
+	if [[ "$selection" =~ ^[0-9]+$ ]] && [ "$selection" -ge 0 ] && [ "$selection" -lt "${#BSP_DIRS[@]}" ]; then
+	    BSP_DIR="${BSP_DIRS[$selection]}"
+	else
+	    echo "Invalid selection."
+	    exit 1
+	fi
+else
+	BSP_DIR="${BSP_DIRS[0]}"
+fi
+
+echo "INFO: BSP_DIR = $BSP_DIR"
+PROJ_DIR=$(dirname "$BSP_DIR")
+
+STANDALONE_DIR="$(find "${PROJ_DIR}" -type d -name standalone)"
+if [ -z "$STANDALONE_DIR" ]; then
+	echo "ERROR: standalone directory is not found in the current BSP"
+	exit 1
+fi
 BOOTLOADER_DIR="$(find "${STANDALONE_DIR}" -type d -name bootloader)"
 BOOTLOADERCONFIG="$(find "${EMBEDDED_SW_DIR}" -type f -name bootloaderConfig.h)"
 SOC_H="$(find "${EMBEDDED_SW_DIR}" -type f -name soc.h)"
@@ -210,5 +234,5 @@ cd $BOOTLOADER_DIR && \
 	fi
 cd - > /dev/null
 
-cp -r $BOOTLOADER_DIR/build $EFINITY_PROJECT_DIR/linux_bootloader
+cp -rf $BOOTLOADER_DIR/build $EFINITY_PROJECT_DIR/linux_bootloader
 echo INFO: The bootloader is in $EFINITY_PROJECT_DIR/linux_bootloader/bootloader.hex
