81aad0bf97d29a8b7a5305b35a2cdc556b5f757a
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2026-03-07 01:15:25 +0800

    init.sh: use reserved_memory_kernel_6_6.json for reserved memory node on kernel 6.6
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- init.sh
+++ init.sh
@@ -412,7 +412,7 @@ function generate_device_tree() {
 
 		# Type specific configs
 		if [[ "$type" == "linux" ]]; then
-			add_cfg_if_exists cmd "$generic_dir/reserved_memory.json"
+			add_cfg_if_exists cmd "$generic_dir/reserved_memory_kernel_6_6.json"
 		fi
 
 		# Features configs
